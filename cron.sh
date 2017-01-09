0690562fa5790d4ad67810a568f72f4a6f20aa5c
Savely Krasovsky <savely@krasovsky.me>
2017-01-09 04:32:41 +0300

    Delete cron.sh

--- cron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-crontab -l > currentjobs
-echo "50 */3 * * * `pwd`/automate.sh defence" >> currentjobs
-echo "*/10 3-5 * * * `pwd`/automate.sh caravan" >> currentjobs
-crontab currentjobs
-rm currentjobs
