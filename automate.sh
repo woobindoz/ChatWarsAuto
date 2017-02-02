e9b4d19f28540d8498c9682f9daff8f3a5a2affd
Savely Krasovsky <savely@krasovsky.me>
2017-02-02 20:35:36 +0300

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -2,27 +2,29 @@
 cd /home/`whoami`/tg
 state="$1"
 if [[ $state = "defence" ]]
-	then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '🛡 Защита'") | bin/telegram-cli -W -v -k tg-server.pub
-	then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '🇻🇦'") | bin/telegram-cli -W -v -k tg-server.pub
+        then
+        (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '  Защита'") | bin/telegram-cli -W -v -k tg-server.pub
+	sleep 120;
+        (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '  '") | bin/telegram-cli -W -v -k tg-server.pub
 fi
 if [[ $state = "caravan" ]]
-	then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '🐫ГРАБИТЬ КОРОВАНЫ'") | bin/telegram-cli -W -v -k tg-server.pub
+        then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 ' ГРАБИТЬ КОРОВАНЫ'") | bin/telegram-cli -W -v -k tg-server.pub
 fi
 if [[ $state = "forest" ]]
-	then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '🌲Лес'") | bin/telegram-cli -W -v -k tg-server.pub
+        then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 ' Лес'") | bin/telegram-cli -W -v -k tg-server.pub
 fi
 if [[ $state = "cave" ]]
-	then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '🕸Пещера'") | bin/telegram-cli -W -v -k tg-server.pub
+        then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 ' Пещера'") | bin/telegram-cli -W -v -k tg-server.pub
 fi
 if [[ $state = "arena" ]]
-	then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '🔎Поиск соперника'") | bin/telegram-cli -W -v -k tg-server.pub
+        then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 ' Поиск соперника'") | bin/telegram-cli -W -v -k tg-server.pub
 fi
 if [[ $state = "go" ]]
-	then bin/telegram-cli -W -v -k tg-server.pub -s scripts/go.lua
+        then bin/telegram-cli -W -v -k tg-server.pub -s scripts/go.lua
 fi
 if [[ $state = "go-att" ]]
-	then bin/telegram-cli -W -v -k tg-server.pub -s scripts/go+att.lua
+        then bin/telegram-cli -W -v -k tg-server.pub -s scripts/go+att.lua
 fi
 if [[ $state = "go-helper" ]]
-	then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '/go'") | bin/telegram-cli -W -v -k tg-server.pub
+        then (sleep 1; echo "contact_list"; sleep 1; echo "msg user#265204902 '/go'") | bin/telegram-cli -W -v -k tg-server.pub
 fi
